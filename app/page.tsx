cf1ca2a49503eccd21f268b94a9b765e4f75ae82
Jarren San Jose <sanjose.jarren@gmail.com>
2025-04-17 22:49:22 -0700

    add コーンチワ

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <h1 className="text-2xl font-bold">cornichiwa 🦄👋</h1>          
+      <main className="flex flex-col gap-[16px] row-start-2 items-center">
+        <h1 className="text-2xl font-bold">cornichiwa 🦄👋</h1>
+        <p className="text-sm text-center">コーンチワ</p>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         <p>footer</p>
